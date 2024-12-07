9ffe3fb341cd16cde2a6600c7db2b57f8d0a0ef2
pokeuq <109028724+pokeuq@users.noreply.github.com>
2024-12-07 18:19:10 +0500

    Сделал задание разработчика 3.

--- app /lr3_var6.py
+++ app /lr3_var6.py
@@ -34,3 +34,72 @@ class Circle:
               "1 разработчик: Мухамедов \n"
               "2 разработчик: Кильмухаметов \n"
               "3 разработчик: Хайриддинов")
+
+class Ball(Circle):
+    def __init__(self, radius, height):
+        super().__init__(radius)
+        try:
+            height = float(height)
+            if height <= 0:
+                print("Ошибка: Высота должна быть положительным числом больше нуля.")
+                self._height = None
+            else:
+                self._height = height
+        except ValueError:
+            print("Ошибка: Высота должна быть числом.")
+            self._height = None
+            
+    @staticmethod
+    def about():
+        print("Программа расчета объема шарового сектора, выполненая командой состоящей из трёх разработчиков:\n"
+              "1 разработчик: Мухамедов \n"
+              "2 разработчик: Кильмухаметов \n"
+              "3 разработчик: Хайриддинов")
+              
+    def sector_volume(self, height=None):
+        if self._radius is None:
+            print("Ошибка: Объем шарового сектора не может быть рассчитан, так как радиус не был корректно задан.")
+            return None
+        
+        if height is None:
+            height = self._height
+            
+        if height is None:
+            print("Ошибка: Высота не задана для расчета объема шарового сектора.")
+            return None
+
+        try:
+            height = float(height)
+            if height <= 0:
+                print("Ошибка: Высота должна быть положительным числом больше нуля.")
+                return None
+            return 2 / 3 * math.pi * self.radius ** 2 * height
+        except (ValueError, TypeError):
+            print("Ошибка: Высота и радиус должны быть числами для вычисления объема шарового сектора.")
+            return None
+
+Ball.about()
+
+while True:
+    try:
+        radius = input("Введите радиус шара (или 'exit' для завершения): ")
+        if radius.lower() == 'exit':
+            print("Программа завершена.")
+            break
+
+        height = input("Введите высоту для расчета сектора (или 'exit' для завершения): ")
+        if height.lower() == 'exit':
+            print("Программа завершена.")
+            break
+
+        if radius and height:
+            ball = Ball(radius, height)
+            sector_volume = ball.sector_volume()
+
+            if sector_volume is not None:
+                print("Объем шарового сектора:", sector_volume)
+        else:
+            print("Радиус и высота должны быть заданы для выполнения расчетов.")
+        print('')
+    except Exception as e:
+        print("Произошла ошибка:", e)
